c2748466d219ab58f1aca0392d35fa7bae28cd53
ssmrxp <146600360+ssmrxp@users.noreply.github.com>
2023-12-21 21:26:17 +0000

    No of Baskets per Household
    No of Baskets per household and the average transaction value per household

--- transaction_data_analysis.sql
+++ transaction_data_analysis.sql
@@ -12,11 +12,50 @@ ORDER BY TOT_Sales ;
 --While we have got 2500 household_keys, we only got 801 rows because, we only have the details of the demographics of 801 household keys. So, it's fair to proceed with just these transactions 
 --keeping in mind the granularity
 
+-- AVG
+Select transaction_data.household_key, AVG(transaction_data.Total_Sales) as TOT_Sales, transaction_data.BASKET_ID, hh_demographic.INCOME_DESC
+FROM transaction_data
+         Inner join hh_demographic
+                    on
+                        transaction_data.household_key = hh_demographic.household_key
+GROUP BY transaction_data.household_key, hh_demographic.INCOME_DESC, transaction_data.BASKET_ID
+ORDER BY BASKET_ID DESC
+
+
+
+-- something(the way I did not calculate the Basket size) is wrong with the above query(FIXED IT) (AVG per visit or per whole basket)
+Select transaction_data.household_key, AVG(transaction_data.Total_Sales) as TOT_Sales, count(DISTINCT transaction_data.BASKET_ID) as Basket_IDS, hh_demographic.INCOME_DESC
+FROM transaction_data
+         Inner join hh_demographic
+                    on
+                        transaction_data.household_key = hh_demographic.household_key
+GROUP BY  transaction_data.household_key, hh_demographic.INCOME_DESC
+ORDER BY TOT_Sales
+
+
+--test:  No if items per Basket for the household_key 239
+Select COUNT(*) as Products_per_Basket, BASKET_ID from transaction_data where household_key = 239 GROUP BY BASKET_ID
 
 
 
 
+--no_of_times_over_2_years
+Select transaction_data.household_key, SUM(transaction_data.Total_Sales) as TOT_Sales, count(DISTINCT transaction_data.BASKET_ID), hh_demographic.INCOME_DESC
+FROM transaction_data
+         Inner join hh_demographic
+                    on
+                        transaction_data.household_key = hh_demographic.household_key
+GROUP BY transaction_data.household_key, hh_demographic.INCOME_DESC
+ORDER BY TOT_Sales
 
+--test: to check the total transaction value with tesco over the span of 2 years) (successful)
+Select sum(Total_Sales) from transaction_data where household_key = 1604 Group by household_key
+
+--test: to check the total no of products per transaction  for the household_key 1604
+Select household_key, COUNT(BASKET_ID) as product_per_transaction, BASKET_ID from transaction_data where household_key = 1604 GROUP BY household_key, BASKET_ID
+
+--test: to check the total no of baskets per household
+Select household_key, COUNT(DISTINCT BASKET_ID), SUM(Total_Sales) BASKET_ID, STORE_ID from transaction_data where household_key = 1604 GROUP BY BASKET_ID, household_key, STORE_ID
 
 -- Want to understand the average Basket Value per Store
 
